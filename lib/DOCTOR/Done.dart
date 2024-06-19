2c639caa53db7cd181f353ffb7b2c46ee775d5bf
ZiadAhmed189 <123209721+ZiadAhmed189@users.noreply.github.com>
2024-06-20 01:46:46 +0300

    update

--- lib/DOCTOR/Done.dart
+++ lib/DOCTOR/Done.dart
@@ -1,4 +1,3 @@
-
 import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,103 +6,145 @@ import 'package:hospital/DATABASE/cubit.dart';
 import 'package:hospital/DATABASE/states.dart';
 import 'package:lottie/lottie.dart';
 
-import '../components.dart';
-
 class done_appointment extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context)=>AppCubit()..getallappointments(),
-      child: BlocConsumer<AppCubit,AppStates>(
-        listener: (context,state){},
-        builder: (context , state)=>Scaffold(
-          body:ConditionalBuilder(
-           condition: state is! AcceptAppointmentsDoctorLoadingState,
-            fallback: (context) => Center(
-              child: CircularProgressIndicator(
-                color: Colors.deepPurple,
-              ),
-            ),
-
-            builder: (context)=>ListView(
-              children: [
-                SizedBox(height: 10.0,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('Appointments,',style: TextStyle(
-                              color: HexColor('32313a'),
-                              fontSize: 15.0,
-                              fontWeight: FontWeight.bold)),
-                          SizedBox(height: 5.0,),
-                          Text('Done',style: TextStyle(
-                              color: HexColor('32313a'),
-                              fontSize: 25.0,
-                              fontWeight: FontWeight.bold)),
-                        ],
-                      ),
-                      CircleAvatar(
-                        backgroundColor: Colors.grey[400],
-                        backgroundImage: AssetImage('asset/image/1.jpg'),
-                        radius: 25.0,
-                      )
-                    ],
-                  ),
+        create: (context) => AppCubit()..getallappointments(),
+        child: BlocConsumer<AppCubit, AppStates>(
+          listener: (context, state) {},
+          builder: (context, state) => Scaffold(
+            body: ConditionalBuilder(
+              condition: state is! AcceptAppointmentsDoctorLoadingState,
+              fallback: (context) => Center(
+                child: CircularProgressIndicator(
+                  color: Colors.deepPurple,
                 ),
-                SizedBox(height: 20.0,),
-                for(int i=0;i<AppCubit.get(context).accept_appointments.length;i++)
-                    Dismissible(
-                      key: Key(AppCubit.get(context).accept_appointments[i].id.toString()),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color:  HexColor('ffe0f4'),
-                            borderRadius: BorderRadius.circular(15),
-                          ),
-
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(children:
-                            [
-                              CircleAvatar(backgroundColor: HexColor('8a86e2'),radius: 35.0,child: Text('${AppCubit.get(context).accept_appointments[i].time}',style: TextStyle(color: Colors.white70,fontSize: 15.0,fontWeight: FontWeight.w500),textAlign: TextAlign.center,),),
-                              SizedBox(width: 20.0,),
-                              Expanded(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    Text('${AppCubit.get(context).accept_appointments[i].patient!.firstName} ${AppCubit.get(context).accept_appointments[i].patient!.lastName}' ,style: TextStyle(color:  HexColor('32313a'),fontSize: 20.0,fontWeight: FontWeight.w600,),),
-                                    Text('${AppCubit.get(context).accept_appointments[i].date}' ,style: TextStyle(color: Colors.grey[800],fontSize: 15.0,fontWeight: FontWeight.w500,),),
-                                  ],
+              ),
+              builder: (context) {
+                if (AppCubit.get(context).accept_appointments.length != 0) {
+                  return ListView(
+                    children: [
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text('Appointments,',
+                                    style: TextStyle(
+                                        color: HexColor('32313a'),
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.bold)),
+                                SizedBox(
+                                  height: 5.0,
                                 ),
-                              ),
-                              Container(
-                                  width: 80.0,
-                                  height: 80.0,
-                                  child: Lottie.network(
-                                      'https://assets5.lottiefiles.com/datafiles/s2s8nJzgDOVLOcz/data.json')),
-                            ]),
-                          ),
+                                Text('Done',
+                                    style: TextStyle(
+                                        color: HexColor('32313a'),
+                                        fontSize: 25.0,
+                                        fontWeight: FontWeight.bold)),
+                              ],
+                            ),
+                            CircleAvatar(
+                              backgroundColor: Colors.grey[400],
+                              backgroundImage: AssetImage('asset/image/1.jpg'),
+                              radius: 25.0,
+                            )
+                          ],
                         ),
                       ),
-                      onDismissed: (direction)
-                      {
-
-                      },
-                    )
-              ],
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      for (int i = 0;
+                          i < AppCubit.get(context).accept_appointments.length;
+                          i++)
+                        Dismissible(
+                          key: Key(AppCubit.get(context)
+                              .accept_appointments[i]
+                              .id
+                              .toString()),
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: HexColor('ffe0f4'),
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(children: [
+                                  CircleAvatar(
+                                    backgroundColor: HexColor('8a86e2'),
+                                    radius: 35.0,
+                                    child: Text(
+                                      '${AppCubit.get(context).accept_appointments[i].time}',
+                                      style: TextStyle(
+                                          color: Colors.white70,
+                                          fontSize: 15.0,
+                                          fontWeight: FontWeight.w500),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: 20.0,
+                                  ),
+                                  Expanded(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        Text(
+                                          '${AppCubit.get(context).accept_appointments[i].patient!.firstName} ${AppCubit.get(context).accept_appointments[i].patient!.lastName}',
+                                          style: TextStyle(
+                                            color: HexColor('32313a'),
+                                            fontSize: 20.0,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                        Text(
+                                          '${AppCubit.get(context).accept_appointments[i].date}',
+                                          style: TextStyle(
+                                            color: Colors.grey[800],
+                                            fontSize: 15.0,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  Container(
+                                      width: 80.0,
+                                      height: 80.0,
+                                      child: Lottie.network(
+                                          'https://assets5.lottiefiles.com/datafiles/s2s8nJzgDOVLOcz/data.json')),
+                                ]),
+                              ),
+                            ),
+                          ),
+                          onDismissed: (direction) {},
+                        )
+                    ],
+                  );
+                } else {
+                  return Center(
+                      child: Text(
+                    "Don't have any appointment",
+                    style: TextStyle(color: Colors.grey, fontSize: 24),
+                  ));
+                }
+              },
             ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
